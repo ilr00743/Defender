2ee93acecb6434293bb18758de1766102e6272c8
illiaR <ilr00743@gmail.com>
2022-11-04 16:16:43 +0200

    Refactoring

--- Assets/Scripts/Units/UnitConfiguration.cs
+++ Assets/Scripts/Units/UnitConfiguration.cs
@@ -1,10 +1,16 @@
-﻿namespace FallingBalls.Units
+﻿using UnityEngine;
+
+namespace FallingBalls.Units
 {
     [System.Serializable]
     public class UnitConfiguration
     {
-        public int MaxHealth;
-        public int MaxSpeed;
-        public int TookDamage;
+        [SerializeField] private int _maxHealth;
+        [SerializeField] private int _tookDamage;
+        [SerializeField] private int _maxSpeed;
+
+        public int MaxHealth => _maxHealth;
+        public int TookDamage => _tookDamage;
+        public int MaxSpeed => _maxSpeed;
     }
 }
